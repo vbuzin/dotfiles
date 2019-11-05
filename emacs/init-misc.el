dc422c38ccef98d87a4a34fa2cbba18062fe47ad
Slava Buzin <v8v.buzin@gmail.com>
2019-11-05 13:05:06 +0100

    don’t ever kill *scratch*

--- emacs/init-misc.el
+++ emacs/init-misc.el
@@ -1,5 +1,13 @@
 ;;; Misc stuff
 ;; =============================================================================
+
+;; Bury the `*scratch*' buffer, never kill it:
+(defadvice kill-buffer (around kill-buffer-around-advice activate)
+  (let ((buffer-to-kill (ad-get-arg 0)))
+    (if (equal buffer-to-kill "*scratch*")
+        (bury-buffer)
+      ad-do-it)))
+
 (when (executable-find "gls")
   (setq insert-directory-program "gls")
   (setq dired-use-ls-dired t)
